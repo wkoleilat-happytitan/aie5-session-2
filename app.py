f74754d0593252afc11dd44281d27827d2b9718c
Walid Koleilat <walid@happytitan.ca>
2025-01-15 20:26:36 -0500

    Update setting up

---
+++ app.py
@@ -0,0 +1,80 @@
+# You can find this code for Chainlit python streaming here (https://docs.chainlit.io/concepts/streaming/python)
+
+# OpenAI Chat completion
+import os
+from openai import AsyncOpenAI  # importing openai for API usage
+import chainlit as cl  # importing chainlit for our app
+from chainlit.prompt import Prompt, PromptMessage  # importing prompt tools
+from chainlit.playground.providers import ChatOpenAI  # importing ChatOpenAI tools
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# ChatOpenAI Templates
+system_template = """You are a helpful assistant to a university level student. You speak in friendly and professional tone. Your answers are easy to understand, comprehensive, and concise. You can put answers in bullet point format when the answer is a list of items.
+"""
+
+user_template = """{input}
+Think through your response step by step. Rate your answers out of 5 at the end as if you are a teacher evaluating answers from students. Suggest follow up questions at the end.
+"""
+
+
+@cl.on_chat_start  # marks a function that will be executed at the start of a user session
+async def start_chat():
+    settings = {
+        "model": "gpt-4",
+        "temperature": 0.7,
+        "max_tokens": 500,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+    }
+
+    cl.user_session.set("settings", settings)
+
+
+@cl.on_message  # marks a function that should be run each time the chatbot receives a message from a user
+async def main(message: cl.Message):
+    settings = cl.user_session.get("settings")
+
+    client = AsyncOpenAI()
+
+    print(message.content)
+
+    prompt = Prompt(
+        provider=ChatOpenAI.id,
+        messages=[
+            PromptMessage(
+                role="system",
+                template=system_template,
+                formatted=system_template,
+            ),
+            PromptMessage(
+                role="user",
+                template=user_template,
+                formatted=user_template.format(input=message.content),
+            ),
+        ],
+        inputs={"input": message.content},
+        settings=settings,
+    )
+
+    print([m.to_openai() for m in prompt.messages])
+
+    msg = cl.Message(content="")
+
+    # Call OpenAI
+    async for stream_resp in await client.chat.completions.create(
+        messages=[m.to_openai() for m in prompt.messages], stream=True, **settings
+    ):
+        token = stream_resp.choices[0].delta.content
+        if not token:
+            token = ""
+        await msg.stream_token(token)
+
+    # Update the prompt object with the completion
+    prompt.completion = msg.content
+    msg.prompt = prompt
+
+    # Send and close the message stream
+    await msg.send()
